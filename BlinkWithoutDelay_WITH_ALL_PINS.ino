bf3990e5ac712c22b0e8a564ffa6638b1b3d723b
Meowchine1 <katevoronina128@gmail.com>
2023-03-31 11:22:24 +0300

    port manipulation

--- BlinkWithoutDelay_WITH_ALL_PINS.ino
+++ BlinkWithoutDelay_WITH_ALL_PINS.ino
@@ -20,101 +20,71 @@ unsigned long previousMillis_9 = 0;
 unsigned long previousMillis_10= 0;  
 unsigned long previousMillis_11 = 0;  
 
-const long interval_3 = micros(1000);  
-const long interval_5 = micros(2000);  
-const long interval_6 = micros(3000);  
-const long interval_9 = micros(4000);  
-const long interval_10 = micros(5000);  
-const long interval_11 = micros(6000);  
+const long interval_3 = 0;  
+const long interval_5 = 0;  
+const long interval_6 = 0;  
+const long interval_9 = 0;  
+const long interval_10 = 0;  
+const long interval_11 = 0;  
 
 void setup() {
-  // set the digital pin as output:
-  pinMode(3, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
+  DDRD |= D00010110 // 26
+  DDRB |=   B00001110  //16
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
-
+  unsigned long currentMillis = micros();
   if (currentMillis - previousMillis_3 >= interval_3) {
-    // save the last time you blinked the LED
     previousMillis_3 = currentMillis;
-
-    // if the LED is off turn it on and vice-versa:
     if (ledState_3 == LOW) {
-      ledState_3 = HIGH;
+      PORTD |= 00001000
     } else {
-      ledState_3 = LOW;
+      PORTD &= 11110111
     }
-    digitalWrite(ledPin_3, ledState_3);
 
 
     if (currentMillis - previousMillis_5 >= interval_5) {
-    // save the last time you blinked the LED
     previousMillis_5 = currentMillis;
-
-    // if the LED is off turn it on and vice-versa:
     if (ledState_5 == LOW) {
-      ledState_5 = HIGH;
+      PORTD |= 00100000
     } else {
-      ledState_5 = LOW;
+      PORTD &= 11011111
     }
-    digitalWrite(ledPin_5, ledState_5);
 
 
     if (currentMillis - previousMillis_6 >= interval_6) {
-    // save the last time you blinked the LED
     previousMillis_6 = currentMillis;
-
-    // if the LED is off turn it on and vice-versa:
     if (ledState_6 == LOW) {
-      ledState_6 = HIGH;
+      PORTD |= 01000000
     } else {
-      ledState_6 = LOW;
+      PORTD &= 10111111
     }
-    digitalWrite(ledPin_6, ledState_6);
 
 
     if (currentMillis - previousMillis_9 >= interval_9) {
-    // save the last time you blinked the LED
     previousMillis_9 = currentMillis;
-
-    // if the LED is off turn it on and vice-versa:
     if (ledState_9 == LOW) {
-      ledState_9 = HIGH;
+      PORTB |= 00000010
     } else {
-      ledState_9 = LOW;
+      PORTB &= 11111101
     }
-    digitalWrite(ledPin_9, ledState_9);
 
 
     if (currentMillis - previousMillis_10 >= interval_10) {
-    // save the last time you blinked the LED
     previousMillis_10 = currentMillis;
-
-    // if the LED is off turn it on and vice-versa:
     if (ledState_10 == LOW) {
-      ledState_10 = HIGH;
+      PORTB |= 00000100
     } else {
-      ledState_10 = LOW;
+      PORTB &= 11111011
     }
-    digitalWrite(ledPin_10, ledState_10);
 
 
     if (currentMillis - previousMillis_11 >= interval_11) {
-    // save the last time you blinked the LED
     previousMillis_11 = currentMillis;
-
-    // if the LED is off turn it on and vice-versa:
     if (ledState_11 == LOW) {
-      ledState_11 = HIGH;
+      PORTB |= 00001000
     } else {
-      ledState_11 = LOW;
+      PORTB &= 11110111
     }
-    digitalWrite(ledPin_11, ledState_11);
   }
 }
